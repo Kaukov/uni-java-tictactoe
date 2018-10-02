67c34119b0259c1e3b6181ee3a02b9f311f61e41
Peter Kaukov <peter.kaukov@gmail.com>
2018-10-02 16:07:05 +0300

    Start working on game tiles. Add basic click functionality

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -1,13 +1,43 @@
 package game;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.util.Arrays;
 
 public class Controller {
     public Label testLabel;
+    public GridPane gameGrid;
+    public Button startGameButton;
+    private boolean isXNext = true;
+    private boolean[][] filledTiles = new boolean[3][3];    // TRUE == filled tile
+    private static final String CLASS_HIDDEN = "hidden";
 
     public void clickTile(ActionEvent actionEvent) {
+        Button clickedButton = (Button) actionEvent.getSource();
+
+        if (clickedButton.getText().isEmpty()) {
+            clickedButton.setText(isXNext ? "X" : "O");
+            isXNext = !isXNext;
+        }
         testLabel.setText("Tile clicked");
-        System.out.println(actionEvent.toString());
+    }
+
+    private void initTiles() {
+        // Clear all tiles
+        for (boolean[] row: filledTiles) {
+            Arrays.fill(row, Boolean.FALSE);
+        }
+    }
+
+    public void startGame() {
+        Button[] gameTiles = new Button[9];
+        Object[] boardTiles = gameGrid.getChildren().toArray();
+
+        startGameButton.setVisible(false);
+
+        initTiles();
     }
 }
