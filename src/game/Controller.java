e4f91320429722550de863c35e95a4486ee5b127
Peter Kaukov <peter.kaukov@gmail.com>
2018-10-02 16:43:38 +0300

    Start following filled tiles and show current player's turn

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -6,30 +6,49 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Controller {
-    public Label testLabel;
+    public Label playerLabel;
     public GridPane gameGrid;
     public Button startGameButton;
+
     private boolean isXNext = true;
-    private boolean[][] filledTiles = new boolean[3][3];    // TRUE == filled tile
+    private char[][] filledTiles = new char[3][3];    // TRUE == filled tile
+
     private static final String CLASS_HIDDEN = "hidden";
+    private static final int START_ROW = 3;
 
     public void clickTile(ActionEvent actionEvent) {
         Button clickedButton = (Button) actionEvent.getSource();
+        int clickedButtonRow = GridPane.getRowIndex(clickedButton) - START_ROW;
+        int clickedButtonColumn = GridPane.getColumnIndex(clickedButton);
 
         if (clickedButton.getText().isEmpty()) {
             clickedButton.setText(isXNext ? "X" : "O");
-            isXNext = !isXNext;
+
+            char newButtonText = clickedButton.getText().toCharArray()[0];
+
+            fillTile(clickedButtonRow, clickedButtonColumn, newButtonText);
+
+            if (checkForWin()) {
+                System.out.println("Congratulations, Player " + newButtonText);
+            } else {
+
+                isXNext = !isXNext;
+
+                setPlayerLabel();
+            }
         }
     }
 
-    private void initTiles() {
-        // Clear all tiles
-        for (boolean[] row: filledTiles) {
-            Arrays.fill(row, Boolean.FALSE);
-        }
+    private void fillTile(int x, int y, char symbol) {
+        filledTiles[x][y] = symbol;
+    }
+
+    private boolean checkForWin() {
+        System.out.println("Checking if one of the players has won...");
+
+        return false;
     }
 
     public void startGame() {
@@ -37,6 +56,8 @@ public class Controller {
         Object[] boardTiles = gameGrid.getChildren().toArray();
 
         startGameButton.setVisible(false);
+
+        setPlayerLabel();
 
         // Get all game tiles
         for (Object tile: boardTiles) {
@@ -49,7 +70,9 @@ public class Controller {
         for (Button gameTile: gameTiles) {
             gameTile.getStyleClass().remove(CLASS_HIDDEN);
         }
+    }
 
-        initTiles();
+    private void setPlayerLabel() {
+        playerLabel.setText("Player " + (isXNext ? "X" : "O") + "'s turn");
     }
 }
